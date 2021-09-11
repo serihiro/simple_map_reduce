10e8c1578aa931274489c5aac87fdb02ef7005f7
Kazuhiro Serizawa <nserihiro@gmail.com>
2021-09-11 13:55:36 +0900

    fix FactoryBot definitions

--- spec/factories/workers.rb
+++ spec/factories/workers.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :worker, class: SimpleMapReduce::Server::Worker do
     transient do
-      url 'http://localhost:4568'
+      url { 'http://localhost:4568' }
     end
 
     initialize_with do
